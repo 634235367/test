7aae7972d08bc63cde12cd5a86f426c066a8e90d
shizhukui <shizhukui@hdsc.com>
2018-04-02 16:06:47 +0800

    local  dev 分支修改

---
+++ src/main/java/nc/testGit.java
@@ -0,0 +1,14 @@
+package nc;
+
+public class testGit {
+  private String name;
+  private String path;
+  public testGit(String name, String path) {
+    super();
+    this.name = name;
+    this.path = path;
+  }
+  
+  
+
+}
