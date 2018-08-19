988566d112230b34ba395fabc52043e0086dc77e
465496918@qq.com <465496918@qq.com>
2018-08-19 23:16:06 +0800

    第一次提交

--- src/main/java/nc/App.java
+++ src/main/java/nc/App.java
@@ -17,7 +17,7 @@ public class App {
   
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Hello World222222222222222222222222!");
+    System.out.println("Hello World2222!");
     System.out.println("Hello 3333333333333333333!");
   }
 }
