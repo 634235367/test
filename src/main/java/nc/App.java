0cbe402329ff225b012aa5fdc1bd931360f9f625
shizhukui <634235367@qq.com>
2018-08-22 00:23:59 +0800

    sd

--- src/main/java/nc/App.java
+++ src/main/java/nc/App.java
@@ -25,8 +25,7 @@ public class App {
     System.out.println("Hello World!");
 
 
-    System.out.println("wwwwwwwwwwwwwwwwwww!");
-    System.out.println("Hdddddddddd!");
+    
     System.out.println("eeeeeeeeeeeeeeeeeee");
      System.out.println("eeeeeeeeeeeeeeeeeee");
      System.out.println("eeeeeeeeeeeeeeeeeee");
