d62261aa0962e7eaf79a546802fdd33e2bdc5d3a
shizhukui <shizhukui@DESKTOP-9E880GS>
2018-08-21 22:43:00 +0800

    szk20180821

--- src/main/java/nc/App.java
+++ src/main/java/nc/App.java
@@ -17,5 +17,9 @@ public class App {
   
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
   }
 }
