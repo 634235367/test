26c933053a00e0f3ef73a872a4ff8a2df06cc6cb
634235367 <634235367@qq.com>
2021-01-26 22:49:27 +0800

    新增文件car

---
+++ src/main/java/zsm/car.java
@@ -0,0 +1,19 @@
+package zsm;
+
+/**
+ * @author szk
+ * @create 2021-01-26
+ */
+public class car {
+
+    private int sex;
+    private String school;
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
