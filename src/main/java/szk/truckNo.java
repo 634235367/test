7b8b6a43d3d23d3608152ad377324f46b47bf916
634235367 <634235367@qq.com>
2021-01-26 23:01:36 +0800

    新增文件

---
+++ src/main/java/szk/truckNo.java
@@ -0,0 +1,13 @@
+package szk;
+
+/**
+ * @author szk
+ * @create 2021-01-26
+ */
+public class truckNo {
+    private String name;
+
+    public truckNo(String name) {
+        this.name = name;
+    }
+}
